1cd8efa49cb9bb4419b0ebfffd5416a9aaa055f5
ruikishimoto <ruikishimoto99@gmail.com>
2023-05-14 01:47:55 +0000

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './style/index.css';
 import Register from './auth/Register';
 import Login from './auth/Login';
@@ -15,18 +15,18 @@ function App() {
 
     return (
         <ChakraProvider theme={theme}>
-            <Box textAlign="center" fontSize="xl">
+            <Box textAlign="center" fontSize="xl" className={"screen"}>
                 <Grid minH="100vh">
                     <BrowserRouter>
                         <Routes>
-                            <Route path="/" element={<HomePage />} />
-                            <Route path="/register" element={<Register />} />
-                            <Route path="/login" element={<Login />} />
-                            <Route path="/VehicleAdd" element={<VehicleForm />} />
-                            <Route path="/VehicleList" element={<List />} />
-                            <Route path="/InsuranceAdd" element={<InsuranceForm />} />
-                            <Route path="/DriverAdd" element={ <DriverForm /> } />
-                            <Route path="/admin" element={ <Admin /> } />
+                            <Route path="/" element={<HomePage/>}/>
+                            <Route path="/register" element={<Register/>}/>
+                            <Route path="/login" element={<Login/>}/>
+                            <Route path="/VehicleAdd" element={<VehicleForm/>}/>
+                            <Route path="/VehicleList" element={<List/>}/>
+                            <Route path="/InsuranceAdd" element={<InsuranceForm/>}/>
+                            <Route path="/DriverAdd" element={<DriverForm/>}/>
+                            <Route path="/admin" element={<Admin/>}/>
                         </Routes>
                     </BrowserRouter>
                 </Grid>
--- frontend/src/Home/index.jsx
+++ frontend/src/Home/index.jsx
@@ -1,79 +1,83 @@
 import Nav from '../components/core/navBar'
+import Footer from '../components/core/footer'
 import {Button, Container, createIcon, Flex, Heading, Stack, Text,} from '@chakra-ui/react';
 
 const Home = () => {
     return (
-        <div className={'h-screen hero'} style={{backgroundImage: "url('/hero.jpg')", backgroundSize: "cover"}}>
-            <Nav/>
-            <Container maxW={'7xl'}>
-                <Stack
-                    align={'center'}
-                    spacing={{base: 8, md: 10}}
-                    py={{base: 20, md: 28}}
-                    direction={{base: 'column', md: 'row'}}>
-                    <Stack flex={1} spacing={{base: 5, md: 10}}>
-                        <Heading
-                            lineHeight={1.1}
-                            fontWeight={600}
-                            fontSize={{base: '3xl', sm: '4xl', lg: '6xl'}}>
-                            <Text
-                                as={'span'}
-                                position={'relative'}
-                                _after={{
-                                    content: "''",
-                                    width: 'full',
-                                    height: '30%',
-                                    position: 'absolute',
-                                    bottom: 1,
-                                    left: 0,
-                                    bg: 'red.400',
-                                    zIndex: -1,
-                                }}>
-                                Umai Car,
-                            </Text>
-                            <br/>
-                            <Text as={'span'} color={'red.400'}>
-                                Zoufriya everywhere!
-                            </Text>
-                        </Heading>
-                        <Text color={'gray.900'}>
-                            Snippy is a rich coding snippets app that lets you create your own
-                            code snippets, categorize them, and even sync them in the cloud so
-                            you can use them anywhere. All that is free!
-                        </Text>
-                        <Stack
-                            spacing={{base: 4, sm: 6}}
-                            direction={{base: 'column', sm: 'row'}}>
-                            <Button
-                                rounded={'full'}
-                                size={'lg'}
-                                fontWeight={'normal'}
-                                px={6}
-                                colorScheme={'red'}
-                                bg={'red.400'}
-                                _hover={{bg: 'red.500'}}>
-                                Get started
-                            </Button>
-                            <Button
-                                rounded={'full'}
-                                size={'lg'}
-                                fontWeight={'normal'}
-                                px={6}
-                                leftIcon={<PlayIcon h={4} w={4} color={'gray.300'}/>}>
-                                How It Works
-                            </Button>
-                        </Stack>
-                    </Stack>
-                    <Flex
-                        flex={1}
-                        justify={'center'}
+        <>
+            <div className={'h-screen hero'} style={{backgroundImage: "url('/hero.jpg')", backgroundSize: "cover"}}>
+                <Nav/>
+                <Container maxW={'7xl'}>
+                    <Stack
                         align={'center'}
-                        position={'relative'}
-                        w={'full'}>
-                    </Flex>
-                </Stack>
-            </Container>
-        </div>
+                        spacing={{base: 8, md: 10}}
+                        py={{base: 20, md: 28}}
+                        direction={{base: 'column', md: 'row'}}>
+                        <Stack flex={1} spacing={{base: 5, md: 10}}>
+                            <Heading
+                                lineHeight={1.1}
+                                fontWeight={600}
+                                fontSize={{base: '3xl', sm: '4xl', lg: '6xl'}}>
+                                <Text
+                                    as={'span'}
+                                    position={'relative'}
+                                    _after={{
+                                        content: "''",
+                                        width: 'full',
+                                        height: '30%',
+                                        position: 'absolute',
+                                        bottom: 1,
+                                        left: 0,
+                                        bg: 'red.400',
+                                        zIndex: -1,
+                                    }}>
+                                    Umai Car,
+                                </Text>
+                                <br/>
+                                <Text as={'span'} color={'red.400'}>
+                                    Zoufriya everywhere!
+                                </Text>
+                            </Heading>
+                            <Text color={'gray.900'}>
+                                Snippy is a rich coding snippets app that lets you create your own
+                                code snippets, categorize them, and even sync them in the cloud so
+                                you can use them anywhere. All that is free!
+                            </Text>
+                            <Stack
+                                spacing={{base: 4, sm: 6}}
+                                direction={{base: 'column', sm: 'row'}}>
+                                <Button
+                                    rounded={'full'}
+                                    size={'lg'}
+                                    fontWeight={'normal'}
+                                    px={6}
+                                    colorScheme={'red'}
+                                    bg={'red.400'}
+                                    _hover={{bg: 'red.500'}}>
+                                    Get started
+                                </Button>
+                                <Button
+                                    rounded={'full'}
+                                    size={'lg'}
+                                    fontWeight={'normal'}
+                                    px={6}
+                                    leftIcon={<PlayIcon h={4} w={4} color={'gray.300'}/>}>
+                                    How It Works
+                                </Button>
+                            </Stack>
+                        </Stack>
+                        <Flex
+                            flex={1}
+                            justify={'center'}
+                            align={'center'}
+                            position={'relative'}
+                            w={'full'}>
+                        </Flex>
+                    </Stack>
+                </Container>
+            </div>
+            <Footer/>
+        </>
     )
 }
 
